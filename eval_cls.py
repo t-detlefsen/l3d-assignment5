50dbcbfae29e30c0f7463a1a3ef9b49dc7ed7497
Thomas Detlefsen <ted32@pitt.edu>
2025-04-05 16:48:20 -0400

    Q1 eval viz

--- eval_cls.py
+++ eval_cls.py
@@ -3,7 +3,7 @@ import argparse
 
 import torch
 from models import cls_model
-from utils import create_dir
+from utils import create_dir, viz_seg
 
 from tqdm import tqdm
 
@@ -64,3 +64,12 @@ if __name__ == '__main__':
     test_accuracy = pred_label.eq(test_label.data).cpu().sum().item() / (test_label.size()[0])
     print ("test accuracy: {}".format(test_accuracy))
 
+    # Correct for each class
+    for i in range(args.num_cls_class):
+        ind = np.where((pred_label == test_label) * (pred_label == i))[0][1]
+        viz_seg(test_data[ind].cpu(), torch.ones(len(test_data[ind])), "{}/cls_{}_{}.gif".format(args.output_dir, pred_label[ind], int(test_label[ind])), args.device)
+
+    # Inorrect for each class
+    for i in range(args.num_cls_class):
+        ind = np.where((pred_label != test_label) * (pred_label == i))[0][0]
+        viz_seg(test_data[ind].cpu(), torch.ones(len(test_data[ind])), "{}/cls_{}_{}.gif".format(args.output_dir, pred_label[ind], int(test_label[ind])), args.device)
