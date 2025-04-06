d7a3313412a7ac12c3567c4b8b9c7aaecc4f29fb
Thomas Detlefsen <ted32@pitt.edu>
2025-04-05 20:39:35 -0400

    Q2 model fix

--- models.py
+++ models.py
@@ -54,13 +54,13 @@ class seg_model(nn.Module):
             nn.Conv1d(3, 64, 1),
             nn.BatchNorm1d(64),
             nn.ReLU(),
+        )
+
+        self.backbone2 = nn.Sequential(
             # Block 2
             nn.Conv1d(64, 128, 1),
             nn.BatchNorm1d(128),
             nn.ReLU(),
-        )
-
-        self.backbone2 = nn.Sequential(
             # Block 3
             nn.Conv1d(128, 1024, 1),
             nn.BatchNorm1d(1024),
@@ -69,19 +69,19 @@ class seg_model(nn.Module):
 
         self.seg = nn.Sequential(
             # Block 1
-            nn.Linear(1088, 512),
+            nn.Conv1d(1088, 512, 1),
             nn.BatchNorm1d(512),
             nn.ReLU(),
             # Block 2
-            nn.Linear(512, 256),
+            nn.Conv1d(512, 256, 1),
             nn.BatchNorm1d(256),
             nn.ReLU(),
             # Block 3
-            nn.Linear(256, 128),
+            nn.Conv1d(256, 128, 1),
             nn.BatchNorm1d(128),
             nn.ReLU(),
             # Output
-            nn.Linear(128, num_seg_classes),
+            nn.Conv1d(128, num_seg_classes, 1),
         )
 
     def forward(self, points):
@@ -91,14 +91,13 @@ class seg_model(nn.Module):
         output: tensor of size (B, N, num_seg_classes)
         '''
         x = points.transpose(2,1)
-        x1 = self.backbone1(x)
-        x = self.backbone2(x1)
-        x = F.max_pool1d(x, points.shape[1]).squeeze(-1)
+        local_features = self.backbone1(x)
+        x = self.backbone2(local_features)
+        global_features = F.max_pool1d(x, points.shape[1]).squeeze(-1)
+        global_features = global_features.unsqueeze(2).repeat(1, 1, points.shape[1])
 
-        import ipdb
-        ipdb.set_trace()
-
-        x = ...
+        x = torch.cat((local_features, global_features), 1)
         x = self.seg(x)
+        x = x.transpose(2,1)
 
         return x
